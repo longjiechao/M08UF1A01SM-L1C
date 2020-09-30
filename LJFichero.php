e84be19b374385f8bd8cb336c58f92624755da9f
LJChao-PC <LJChao-PC@DESKTOP-7DOO4BM>
2020-09-30 19:35:20 +0200

    lc - creacion fichero LJFichero + 3 funciones

---
+++ LJFichero.php
@@ -0,0 +1,30 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        
+        function LJfunc1(){
+            
+        }
+        function LJfunc2(){
+            
+        }
+        function LJfunc3(){
+            
+        }
+        
+        
+        ?>
+    </body>
+</html>
